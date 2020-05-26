69ace21467ee696e5606432d304f8d45e10d4b36
Elayne Argollo <48841005+elayneargollo@users.noreply.github.com>
2020-05-26 10:55:20 -0300

    Update

--- Modulo01/Aula15 - Requisição HTTP/Aula15 - Requisição HTTP/JS/script.js
+++ Modulo01/Aula15 - Requisição HTTP/Aula15 - Requisição HTTP/JS/script.js
@@ -1,19 +1,38 @@
-window.addEventListener('load', function(){
-        fetch("https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo").then((res) => {
+/*window.addEventListener('load', function(){
+     fetch("https://randomuser.me/api/?seed=javascript&https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo=100&nat=BR&noinfo").then((res) => {
         res.json().then(data => {
-            doMap(data);
+            doShow(data);
         });
+    }).catch(erro => {
+    
+    let myAlertText = document.createTextNode("Erro na requisição ... ");
+    document.body.appendChild(myAlertText); 
+
     });
+});*/
+
+
+window.addEventListener('load', () =>{
+    doFetchAsync();
 });
 
-function doMap(data){
-    const nameArray =  data.results.map(person => {
-        return {
-            name: person.name.first,
-            surName: person.name.last,
-            age: person.dob.age
-        };
-    });
+async function doFetchAsync(){
+    const res = await fetch("https://randomuser.me/api/?seed=javascript&https://randomuser.me/api/?seed=javascript&results=100&nat=BR&noinfo=100&nat=BR&noinfo");
+    const json = await res.json();
 
-    console.log(nameArray);
+    doShow(json);
+}   
+
+function doShow(data){
+      
+    let myAlertText = document.createTextNode('Soma das idades: ' +doReduceAge(data));
+    document.body.appendChild(myAlertText); 
+}
+
+function doReduceAge(data){
+    const totalAges = data.results.reduce((accumulator, current) =>{
+        return accumulator + current.dob.age;
+    },0);
+
+    return totalAges;
 }
