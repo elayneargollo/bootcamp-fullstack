280ef6f989a62ed41ee09e3210912a85dcad50a0
Elayne Argollo <48841005+elayneargollo@users.noreply.github.com>
2020-06-07 08:48:08 -0300

    Modulo 2

---
+++ Modulo02/Aula01/JS/script.js
@@ -0,0 +1,7 @@
+// importa o modulo, ou seja, outro js
+var teste = require('./test-module');
+
+// aqui eu chamo as funções que estão no modulo importado
+console.log(teste.testFunction());
+console.log(teste.testsoma());
+
---
+++ Modulo02/Aula01/JS/test-module.js
@@ -0,0 +1,7 @@
+exports.testFunction = function () {
+    return 'teste function done';
+};
+
+exports.testsoma = function (){
+    return 2+8;
+}
