1d9f3937202255c46f4c33a03ed287f949b38d3e
Elayne Argollo <48841005+elayneargollo@users.noreply.github.com>
2020-06-14 15:05:51 -0300

    delete

--- Modulo02/Aula04/JS/script.js
+++
@@ -1,8 +0,0 @@
-const express = require('express'); // importação do Express
-const app = express(); // será utilizado para configurar a aplicação
-const port = 3000; // porta utilizada pela API
-
-// app é uma instânica do Express
-// definição da rota
-app.get('/', (req, res) => res.send('Hello World!')); 
-app.listen(port, () => console.log('App listening on port ${port}"'));
