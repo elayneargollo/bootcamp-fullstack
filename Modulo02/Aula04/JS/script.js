8eb6a72c251120c3e2cda6f1cc52027c3c20227d
Elayne Argollo <48841005+elayneargollo@users.noreply.github.com>
2020-06-14 16:14:30 -0300

    Update script.js

--- Modulo02/Aula04/JS/script.js
+++ Modulo02/Aula04/JS/script.js
@@ -2,7 +2,11 @@ const express = require('express'); // importação do Express
 const app = express(); // será utilizado para configurar a aplicação
 const port = 3000; // porta utilizada pela API
 
-// app é uma instânica do Express
-// definição da rota
-app.get('/', (req, res) => res.send('Hello World!')); 
+
+/* app é uma instancia do express, 
+get é um método http,
+/teste é o caminho que a rota irá responder,
+função será executada quando a rota for atingida */
+
+app.get('/', (req, res) => res.send('Hello World!')); /* localhost:3000/teste */
 app.listen(port, () => console.log('App listening on port ${port}"'));
