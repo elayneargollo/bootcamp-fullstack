efd68ed9973ffc511cf365e4ef43e7ee24b284db
elayneargollo <elayne.natalia@outlook.com>
2020-06-17 20:49:08 -0300

    JavaScript Performático

---
+++ Modulo03 - React/Aula02/JavaScriptPerformatico/JS/timestamp.js
@@ -0,0 +1,37 @@
+function leftPad(value, count = 2, char = '0'){
+    
+   let newValue = value.toString();
+    
+    if(value.toString.length < count){
+        for(let i=0; i < count - value.toString().length; i++){
+            newValue = char + value.toString();
+        }
+    }
+
+    return newValue;
+}
+
+function getNewTimestamp(){
+
+    const now = new Date();
+    let result = '';
+
+    result += 'Data: '
+    result += leftPad(now.getDate());
+    result += '/';
+    result += leftPad(now.getMonth() + 1);
+    result += '/';
+    result += leftPad(now.getFullYear());
+    result += ' ';
+    result += 'Hora: '
+    result += leftPad(now.getHours());
+    result += ':';
+    result += leftPad(now.getMinutes());
+    result += ':';
+    result += leftPad(now.getSeconds());
+    result += '.';
+    result += leftPad(now.getMilliseconds(),3);
+
+    return result;
+
+}
